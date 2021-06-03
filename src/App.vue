dc6c8470327f8895c23b173478d7b37e9d088ef3
Timoking <13466083337@163.com>
2021-06-03 18:16:36 +0800

    base layout

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,92 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="head">
+      <div id="logo">
+        <div id="img">
+          <img src="@/assets/农行-21.png"/>
+        </div>
+      </div>
+      <div id="tabs"></div>
     </div>
-    <router-view/>
+    <div id="main">
+      <div id="nav">
+        <div class="nav-option"></div>
+        <div class="nav-option"></div>
+        <div class="nav-option"></div>
+      </div>
+      <div id="article">
+        <router-view/>
+      </div>
+    </div>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+
+export default {
+  name: 'app',
 }
+</script>
 
-#nav {
-  padding: 30px;
+<style lang="scss">
+#head{
+  height: 1.67rem;
+  width: 100%;
+  border-bottom: 1px rgba($color: #000000, $alpha: 0.5) solid;
+  display: flex;
+  margin-bottom: 0.37rem;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+  #logo{
+    height: 100%;
 
-    &.router-link-exact-active {
-      color: #42b983;
+    #img{
+      width: 3.44rem;
+      height: 1.09rem;
+      margin: 0.34rem 0 0.24rem 0.57rem;
+
+      img{
+        max-width: 100%;
+        height: auto;
+      }
     }
   }
+
+  #tabs{
+    height: 100%;
+    width: 100%;
+    border-left: 1px solid black;
+  }
+}
+
+#main{
+  width: 100%;
+  height: 85%;
+  display: flex;
+
+  #nav{
+    width: 3.79rem;
+    overflow: hidden;
+    padding-top: 0.06rem;
+
+    .nav-option{
+      width: 2.95rem;
+      height: 1.03rem;
+      margin: 0 0.36rem 0.48rem 0.48rem;
+      
+      border-radius: 10px;
+      box-shadow: 0 0 0.06rem 0.06rem rgba($color: rgb(204, 199, 199), $alpha: 0.5);
+    }
+  }
+
+  #article{
+    padding-top: 0.06rem;
+  }
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  height: 100vh;
 }
 </style>
